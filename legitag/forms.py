7b372e1f7041cd72a230f46157d5b5897e6f9ccb
Richard Pope <richardpope@MacBook-Pro.local>
2016-07-08 14:26:04 +0100

    copy

--- legitag/forms.py
+++ legitag/forms.py
@@ -3,9 +3,9 @@ from wtforms.widgets import TextArea
 from flask_security.forms import RegisterForm
 
 class TagForm(Form):
-    policy_area_tags = TextField('What policy areas might it cover?', widget=TextArea())
-    users_affected_tags = TextField('What types of person, groups or organisation might it affect?', widget=TextArea())
-    organisation_tags = TextField('What specific, named organisations might it affect?', widget=TextArea())
+    policy_area_tags = TextField('What policy areas might it cover?', widget=TextArea(), description="eg farming, workers rights, groundwater management")
+    users_affected_tags = TextField('What types of person, groups or organisation might it affect?', widget=TextArea(), description="eg farmer, supermarket, police, car manufacturer")
+    organisation_tags = TextField('What specific, named organisations might it affect?', widget=TextArea()description="eg Lloyd’s Register, Royal Pharmaceutical Society, City of London")
 
 class RegisterUserForm(RegisterForm):
     name = StringField('Name', [validators.Required()])
